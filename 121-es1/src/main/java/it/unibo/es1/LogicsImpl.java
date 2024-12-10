0264e757e019987ee065c932beab05e00e613ac3
Alice Ferri - alice.ferri8@studio.unibo.it <alice.ferri8@studio.unibo.it>
2024-12-10 17:05:15 +0100

    Ex 121

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,79 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogicsImpl implements Logics {
 
+	private final int size;
+	private List<OneButton> listButtons = new ArrayList<>();
+
+	private class OneButton{
+		private int count;
+		private boolean enable;
+
+		public OneButton(final int count, final boolean enable){
+			this.count = count;
+			this.enable = enable;
+		}
+
+		public int getCount() {
+			return count;
+		}
+
+		public boolean isEnable() {
+			return enable;
+		}
+
+		public void setCount() {
+			this.count++;
+		}
+
+		public void setNotEnable() {
+			this.enable = false;
+		}
+	}
+
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		this.size = size;
+
+		for(int i = 0; i < size; i++){
+			listButtons.add(new OneButton(0, true));
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.size;
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return listButtons.stream().map(v -> v.getCount()).toList();
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return listButtons.stream().map(v -> v.isEnable()).toList();
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		listButtons.get(elem).setCount();
+		if (listButtons.get(elem).getCount() == this.size) {
+			listButtons.get(elem).setNotEnable();
+		}		
+		return listButtons.get(elem).getCount();
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return listButtons.stream().map(v -> String.valueOf(v.getCount())).collect(Collectors.joining("|", "<<", ">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return listButtons.stream().allMatch(v -> v.getCount() == listButtons.getFirst().getCount());
 	}
 }
